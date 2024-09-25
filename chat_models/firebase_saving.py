205779f1d9257b479161faf690970dd258044d89
Pramodya Sahan Dilushana <sahandil1013@gmail.com>
2024-09-25 15:56:53 +0530

    feat: integrate Firestore chat history with OpenAI model interaction

---
+++ chat_models/firebase_saving.py
@@ -0,0 +1,43 @@
+from dotenv import load_dotenv
+from google.cloud import firestore
+from langchain_google_firestore import FirestoreChatMessageHistory
+from langchain_openai import ChatOpenAI
+
+
+load_dotenv()
+
+# Setup Firebase Firestore
+PROJECT_ID = "chatbot-fc87e"
+SESSION_ID = "user_session_id"
+COLLECTION_NAME = "chat_history"
+
+# Initialize Firestore Client
+print("Initializing Firestore Client...")
+client = firestore.Client(project=PROJECT_ID)
+
+# Initialize Firestore Chat Message History
+print("Initializing Firestore Chat Message History...")
+chat_history = FirestoreChatMessageHistory(
+    session_id=SESSION_ID,
+    collection=COLLECTION_NAME,
+    client=client,
+)
+print("Chat History Initialized.")
+print("Current Chat History:", chat_history.messages)
+
+# Initialize Chat Model
+model = ChatOpenAI()
+
+print("Start chatting with the AI. Type 'exit' to quit.")
+
+while True:
+    human_input = input("User: ")
+    if human_input.lower() == "exit":
+        break
+
+    chat_history.add_user_message(human_input)
+
+    ai_response = model.invoke(chat_history.messages)
+    chat_history.add_ai_message(ai_response.content)
+
+    print(f"AI: {ai_response.content}")
