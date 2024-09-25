22563be9a9cff648d581c90df03c4cbc5bffbb6c
Pramodya Sahan Dilushana <sahandil1013@gmail.com>
2024-09-25 16:22:10 +0530

    feat: add multi-message prompt template for generating multiple jokes with dynamic topics and counts

--- prompt_template/basic.py
+++ prompt_template/basic.py
@@ -1,11 +1,23 @@
 from langchain.prompts import ChatPromptTemplate
-from langchain_core.messages import HumanMessage
 
 template = "Tell me a joke about {topic}."
 prompt_template = ChatPromptTemplate.from_template(template)
 
 prompt = prompt_template.invoke({
     "topic": "cats",
+})
+
+print(prompt)
+
+messages = [
+    ("system", "You are a comedian who tells jokes about {topic}."),
+    ("human", "Tell me {joke_count} jokes.")
+]
+
+prompt_template = ChatPromptTemplate.from_messages(messages)
+prompt = prompt_template.invoke({
+    "topic": "engineer",
+    "joke_count": 3,
 })
 
 print(prompt)
